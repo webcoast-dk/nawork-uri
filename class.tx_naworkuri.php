69b785ffe2361cd48378f52b0793ccce5f5c3e40
martin <martin@4e038621-a2e2-0310-8a16-b571fa1495e2>
2008-12-23 13:03:24 +0000

    removed unnecessary comments
    git-svn-id: https://svn.work.de/svn/typo3/nawork_uri/trunk@2069 4e038621-a2e2-0310-8a16-b571fa1495e2

--- class.tx_naworkuri.php
+++ class.tx_naworkuri.php
@@ -71,55 +71,10 @@ class tx_naworkuri {
       			exit;
     		} 
 		}
-		
-		/*
-	  if (empty($_GET['ADMCMD_prev']) && $GLOBALS['TSFE']->config['config']['tx_cooluri_enable']==1 && $GLOBALS['TSFE']->config['config']['redirectOldLinksToNew']==1 && $GLOBALS['TSFE']->siteScript && (substr($GLOBALS['TSFE']->siteScript,0,9)=='index.php' || substr($GLOBALS['TSFE']->siteScript,0,1)=='?')) {
-	    
-	    $ss = explode('?',$GLOBALS['TSFE']->siteScript);
-	    if ($ss[1]) $pars = Link_Func::convertQuerystringToArray($ss[1]);
-	    
-	    $params = Array();
-	    
-	    if ($pars) {
-	      $lt = $this->getTranslateInstance();
-	      //var_dump($lt);
-	      if (!$lt) return;
-	
-	      $url = $lt->params2cool($pars);
-	      
-	      if ($this->confArray['MULTIDOMAIN']) {
-	        $url = explode('@',$url);  
-	        $url = 'http://'.$url[0].'/'.$url[1];
-	      }
-	      
-	      Link_Func::redirect($url);
-	    }
-	  }
-	*/ 
-		
-		
-		/*
-	$path = preg_replace('~^/~','',$path);
-    if (dirname($_SERVER['PHP_SELF'])!='' && dirname($_SERVER['PHP_SELF'])!='/' && dirname($_SERVER['PHP_SELF'])!='\\') {
-      $path = dirname($_SERVER['PHP_SELF']).'/'.$path;
-    }
-    $path = preg_replace('~^/~','',$path);
-    if (!preg_match('~^http://~',$path)) {
-      $path = 'http://'.$_SERVER['HTTP_HOST'].'/'.$path;
-    }
-      // avoid &amp; entities in location url
-    $path = html_entity_decode($path);
-    
-    if( !($_SERVER['REQUEST_METHOD']=='POST' && preg_match('/index.php/', $_SERVER['SCRIPT_NAME'])) ) {
-      header('Location: '.$path,true,301);
-      exit;
-    } 
-	 */
-	  
 	}
 
 		/**
-		 * Update the md5 value automatically
+		 * Update the md5 values automatically
 		 *
 		 * @param unknown_type $incomingFieldArray
 		 * @param unknown_type $table

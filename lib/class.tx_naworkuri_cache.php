2c1a41db352499e921709922473fc417dcba50f8
martin <martin@4e038621-a2e2-0310-8a16-b571fa1495e2>
2009-01-07 13:30:07 +0000

    added debug infos
    git-svn-id: https://svn.work.de/svn/typo3/nawork_uri/trunk@2112 4e038621-a2e2-0310-8a16-b571fa1495e2

--- lib/class.tx_naworkuri_cache.php
+++ lib/class.tx_naworkuri_cache.php
@@ -11,7 +11,11 @@ class tx_naworkuri_cache {
 	 * @return string : uri wich matches to these params otherwise false 
 	 */
 	public function read ($id, $lang, $domain, $parameters){
-
+			// sort parameters
+		$exploded_params = explode('&',$parameters);
+		sort($exploded_params);
+		$parameters = implode($exploded_params);
+			// lookup in db
 		$dbres = $GLOBALS['TYPO3_DB']->exec_SELECTquery('path', 'tx_naworkuri_uri', 'deleted=0 AND pid='.$id.' AND sys_language_uid='.$lang.' AND domain="'.$domain.'" AND hash_params LIKE "'.md5($parameters).'"' );
 		if ( $row=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($dbres) ){
 			return $row['path'];
@@ -28,9 +32,13 @@ class tx_naworkuri_cache {
 	
 	// public function write($params, $uri){
 	public function write($id, $lang, $domain, $parameters, $path, $debug_info = ''){
-		
+			// sort parameters
+		$exploded_params = explode('&',$parameters);
+		sort($exploded_params);
+		$parameters = implode($exploded_params);
+			// make uri unique
 		$path = $this->unique($path, $domain);
-		
+			// save in dm
 		$save_record = array(
 			'pid' => $id,
 			'sys_language_uid' =>  $lang,

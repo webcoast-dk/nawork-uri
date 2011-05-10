f9ea5086ee6cb5fbbea6bd3ac8059b2856766eae
marten <marten@4e038621-a2e2-0310-8a16-b571fa1495e2>
2011-05-10 11:21:50 +0000

    [UPDATE] Workaround for safe_mode and open_basedir
    * added function for environments where safe_mode or open_basedir is set
    
    git-svn-id: https://svn.work.de/svn/typo3/nawork_uri/trunk@4419 4e038621-a2e2-0310-8a16-b571fa1495e2

--- class.tx_naworkuri.php
+++ class.tx_naworkuri.php
@@ -51,8 +51,13 @@ class tx_naworkuri {
 									curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
 									curl_setopt($curl, CURLOPT_TIMEOUT, 30);
 									curl_setopt($curl, CURLOPT_USERAGENT, 'nawork_uri');
-									curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-									$res = curl_exec($curl);
+									// workaround for open_basedir and safe mode
+									if ( ini_get('open_basedir') == '' && ( ini_get('safe_mode' == 'Off') || ini_get('safe_mode' == '') ) ){
+										curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+										$res = curl_exec($curl);
+									} else {
+										$res = $this->curl_redir_exec($curl);
+									}
 								} else {
 									$res = '404 not found! The 404 Page URL ' . $configReader->getPageNotFoundConfigBehaviorValue() . ' seems to cause a redirect loop.';
 								}
@@ -201,6 +206,57 @@ class tx_naworkuri {
 				$incomingFieldArray['hash_path'] = md5($incomingFieldArray['path']);
 			if ($incomingFieldArray['params'] || $incomingFieldArray['params'] == '')
 				$incomingFieldArray['hash_params'] = md5($incomingFieldArray['params']);
+		}
+	}
+	
+	
+	private function curl_redir_exec($ch) {
+		
+		static $curl_loops = 0;
+		static $curl_max_loops = 20;
+
+		if ($curl_loops++ >= $curl_max_loops) {
+			$curl_loops = 0;
+			return FALSE;
+		}
+		
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($ch);
+
+		list($header, $data) = explode("\n\n", $data, 2);
+		$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		if ($http_code == 301 || $http_code == 302) {
+			$matches = array();
+			preg_match('/Location:(.*?)\n/', $header, $matches);
+			$url = @parse_url(trim(array_pop($matches)));
+
+			if (!$url) {
+				//couldn't process the url to redirect to
+				$curl_loops = 0;
+				return $data;
+			}
+
+			$last_url = parse_url(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL));
+
+			if (!$url['scheme'])
+				$url['scheme'] = $last_url['scheme'];
+			if (!$url['host'])
+				$url['host'] = $last_url['host'];
+			if (!$url['path'])
+				$url['path'] = $last_url['path'];
+
+			$new_url = $url['scheme'] . '://' . $url['host'] . $url['path'] . ($url['query'] ? '?' . $url['query'] : '');
+
+			curl_setopt($ch, CURLOPT_URL, $new_url);
+
+			return $this->curl_redir_exec($ch);
+		} else {
+
+			$curl_loops = 0;
+
+			return $data;
 		}
 	}
 

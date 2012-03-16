feb953feece2b58503474bc772c4d6a1871c28c1
Thorben Kapp <thorben@work.de>
2012-03-16 16:27:02 +0100

    [FEATURE] New configuration syntax and reader
    * move the default configuration to new directory
    * add new configuration reader an object

---
+++ Classes/Configuration/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Configuration
+ *
+ * @author thorben
+ */
+class Tx_NaworkUri_Configuration_Configuration {
+	
+}
+
+?>

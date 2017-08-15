6abbbaf36c1c8e7cbf9ec1bd260ab928a4527d1e
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2017-08-15 15:40:34 +0200

    [BUGFIX] Request urls without <Append> not redirected
    Instead of accepting request with urls that are missng the <Append> value
    and showing the page, send a redirect to avoid duplicate content.

--- Classes/Exception/UrlIsRedirectException.php
+++ Classes/Exception/UrlIsRedirectException.php
@@ -9,13 +9,24 @@ namespace Nawork\NaworkUri\Exception;
  */
 class UrlIsRedirectException extends \Exception {
 
+    /**
+     * @var array
+     */
 	protected $url;
 
+    /**
+     * UrlIsRedirectException constructor.
+     *
+     * @param array $url
+     */
 	public function __construct($url) {
 		parent::__construct('The url is a redirect');
 		$this->url = $url;
 	}
 
+    /**
+     * @return array
+     */
 	public function getUrl() {
 		return $this->url;
 	}
